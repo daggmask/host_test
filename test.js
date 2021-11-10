a3be2985b7b47eaf2c855e7acad5d02035b8e8bc
Martin Hellström <56336568+daggmask@users.noreply.github.com>
2021-11-10 15:13:37 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+include("https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js")
+include("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js")
+include("https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js")
+include("https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js")
+
+let printHelloWorld = () => {
+    console.log('Hello world')
+}

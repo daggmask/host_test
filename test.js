b9f25dbe5d772b45ad65224ccf5485f60a0a96c0
Martin Hellström <56336568+daggmask@users.noreply.github.com>
2021-11-10 15:18:15 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,3 @@
-<script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js"></script>
-</script>        
-
 let printHelloWorld = () => {
     console.log('Hello world')
 }
